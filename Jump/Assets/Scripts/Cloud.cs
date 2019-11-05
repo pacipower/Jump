f1d3a701511965d8e563fcaa0335ae1aa5897994
pacipower <pacipower7@gmail.com>
2019-11-05 16:46:18 +0100

    add background

---
+++ Jump/Assets/Scripts/Cloud.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    float y,z;
+    private float move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        y = transform.position.y;
+        z = transform.position.z;
+        move = -0.01f / z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x<-10)
+        {
+            transform.position = new Vector3(10, transform.parent.position.y-1+y, z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += Vector3.right * move;
+    }
+}
